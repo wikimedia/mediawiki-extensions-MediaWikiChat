4451605818bebda6ce60a94b8e24cf04693b61c0
UltrasonicNXT <adamr_carter@btinternet.com>
2014-01-11 16:37:32 +0000

    Fix more problems with not-safed usernames
    They were causing endless problems
    
    Change-Id: I362978672e428f56cb73b8279de893b0a6599f50

--- MediaWikiChat.js
+++ MediaWikiChat.js
@@ -18,7 +18,7 @@ var MediaWikiChat = {
 	},
 
 	safe: function( string ) {
-		return string.replace( /\(\)\/\\\[\]\{\}\.\*\+\?\^=!:$\|/g, '' ).replace( / /g, '' );
+		return string.replace( /[^\w\s]|/g, '' ).replace( / /g, '' );
 	},
 
 	unique: function( array ) {
