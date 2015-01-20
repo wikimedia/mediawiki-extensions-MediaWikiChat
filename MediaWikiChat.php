3365d4bc15685ce8a177d1495fbce46665e152b1
UltrasonicNXT <adamr_carter@btinternet.com>
2015-01-20 07:17:08 +0000

    Don't auto scroll when the user is scrolled up
    Very annoying!
    
    Plus some other cleanup
    
    Change-Id: I5ae504a853aea2af6cd61bdc6f12e34888b3aba2

--- MediaWikiChat.php
+++ MediaWikiChat.php
@@ -16,7 +16,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'MediaWikiChat',
-	'version' => '2.14.2',
+	'version' => '2.14.3',
 	'author' => 'Adam Carter/UltrasonicNXT',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:MediaWikiChat',
 	'descriptionmsg' => 'chat-desc',
