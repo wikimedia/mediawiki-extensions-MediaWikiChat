87dd0b33d96038c4e7ae62dbe2fb259198abce01
UltrasonicNXT <adamr_carter@btinternet.com>
2015-11-13 21:35:10 +0000

    Some message changes
    URL change not ideal, but easiest way to do it AFAIK
    
    Change-Id: Ia5ffd7f366b5e35963b57dc02083784c31813a50

--- MediaWikiChat.php
+++ MediaWikiChat.php
@@ -16,7 +16,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'MediaWikiChat',
-	'version' => '2.16.1',
+	'version' => '2.16.2',
 	'author' => 'Adam Carter/UltrasonicNXT',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:MediaWikiChat',
 	'descriptionmsg' => 'chat-desc',
