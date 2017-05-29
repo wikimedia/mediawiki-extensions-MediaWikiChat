e70493df7119aaa028daaf8779fee8d494022360
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-05-28 22:39:01 -0500

    Register MediaWiki:Chat.css/.js as ResourceLoaderWikiModules
    Change-Id: I43274239a735327b8f3331a5bb0d5f58bc711f65

---
+++ modules/MediaWikiChatWikiModule.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class MediaWikiChatWikiModule extends ResourceLoaderWikiModule {
+	/**
+	 * Get a list of pages used by this module.
+	 *
+	 * @param ResourceLoaderContext $context
+	 * @return array
+	 */
+	protected function getPages( ResourceLoaderContext $context ) {
+		return [
+			'MediaWiki:Chat.css' => [ 'type' => 'style' ],
+			'MediaWiki:Chat.js' => [ 'type' => 'script' ],
+		];
+	}
+}
