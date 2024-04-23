wikimedia/mediawiki-extensions-MediaWikiChat

commit 6c5a368c2fe2429d5055a489d0ba01929a566de3

Random cleanup
* Use $wgExtensionAssetsPath instead of $wgScriptPath + extensions/ in the JS file for building URLs
* Mark function visibilities
* Coding style tweaks
* Tweak variable names for clarity
* Reduce nesting by one level in MediaWikiChat#getOnline by bailing out right away if the user lacks the 'chat' user right
* Add missing __METHOD__ to some database calls
* Remove unnecessary 3rd parameter to DatabaseUpdater#addExtensionTable
* Rename hooked functions fnNewSidebarItem and wfPrefHook and briefly document the latter
* Rename UserGroupsChanged hook handler to onUserGroupsChanged instead of onUserGroups, for consistency
* Capitalize SQL keywords like COUNT(*) properly
* Variabilize a call to MediaWikiServices::getInstance() instead of calling it twice
* Remove .ie() method from the JS file, because:
** It's unused, it was added in 19e5ff7cd186347f39858ec227077f19bb5dbab4 but was never used (AFAIK)
** Internet Explorer isn't supported by MediaWiki anymore anyway
* Likewise, get rid of some IE-only CSS styles in MediaWikiChatHooks.php and MediaWikiChat.js
* Add a link to Special:UserLogin to the "chat-not-allowed" i18n msg - tiny usability/quality-of-life improvement

Change-Id: I1b69f0fa9a57820f197fafe2af6d878cb8f7bbf0

includes/api/GetNewWorker.php
@@ -12,6 +12,7 @@ class GetNewWorker {
 	 */
 	static function execute( ApiResult $result, User $user, ApiMain $main ) {
 		global $wgChatOnlineTimeout;
+
 		$dbr = wfGetDB( DB_REPLICA );
 		$dbw = wfGetDB( DB_PRIMARY );
 		$mName = 'chatgetnew';
@@ -72,7 +73,6 @@ class GetNewWorker {
 			}
 
 			if ( $row->chat_type == MediaWikiChat::TYPE_MESSAGE ) {
-
 				$id = $row->chat_user_id;
 				$message = $row->chat_message;
 
@@ -80,7 +80,6 @@ class GetNewWorker {
 				$result->addValue( [ $mName, 'messages', $timestamp ], '*', $message );
 
 				$users[$id] = true; // ensure message sender is in users list
-
 			} elseif ( $row->chat_type == MediaWikiChat::TYPE_PM
 				&& (
 					$row->chat_user_id == $user->getId()
@@ -89,21 +88,21 @@ class GetNewWorker {
 
 				$message = $row->chat_message;
 
-				$fromid = $row->chat_user_id;
+				$fromId = $row->chat_user_id;
 				$toid = $row->chat_to_id;
 
-				if ( $fromid == $user->getId() ) {
-					$convwith = User::newFromId( $toid )->getName();
+				if ( $fromId == $user->getId() ) {
+					$convWith = User::newFromId( $toId )->getName();
 				} else {
-					$convwith = User::newFromId( $fromid )->getName();
+					$convWith = User::newFromId( $fromId )->getName();
 				}
 
 				$result->addValue( [ $mName, 'pms', $timestamp ], '*', $message );
-				$result->addValue( [ $mName, 'pms', $timestamp ], 'from', $fromid );
-				$result->addValue( [ $mName, 'pms', $timestamp ], 'conv', $convwith );
+				$result->addValue( [ $mName, 'pms', $timestamp ], 'from', $fromId );
+				$result->addValue( [ $mName, 'pms', $timestamp ], 'conv', $convWith );
 
-				$users[$fromid] = true; // ensure pm sender is in users list
-				$users[$toid] = true; // ensure pm receiver is in users list
+				$users[$fromId] = true; // ensure PM sender is in users list
+				$users[$toId] = true; // ensure PM receiver is in users list
 
 			} elseif ( $row->chat_type == MediaWikiChat::TYPE_KICK ) {
 				if ( $row->chat_to_id == $user->getId() ) {
@@ -130,24 +129,31 @@ class GetNewWorker {
 		foreach ( $onlineUsers as $id => $away ) {
 			$users[$id] = true; // ensure all online users are present in the users list
 		}
-		$genderCache = MediaWikiServices::getInstance()->getGenderCache();
-		$userGroupManager = MediaWikiServices::getInstance()->getUserGroupManager();
+
+		$services = MediaWikiServices::getInstance();
+		$genderCache = $services->getGenderCache();
+		$userGroupManager = $services->getUserGroupManager();
+
 		foreach ( $users as $id => $tr ) {
 			$userObject = User::newFromId( $id );
 			$idString = strval( $id );
 
 			$result->addValue( [ $mName, 'users', $idString ], 'name', $userObject->getName() );
+
 			if ( class_exists( 'SocialProfileHooks' ) ) { // is SocialProfile installed?
 				$result->addValue( [ $mName, 'users', $idString ], 'avatar', MediaWikiChat::getAvatar( $id ) );
 			}
+
 			if ( array_key_exists( $id, $onlineUsers ) ) {
 				$result->addValue( [ $mName, 'users', $idString ], 'online', 'true' );
 				$result->addValue( [ $mName, 'users', $idString ], 'away', $onlineUsers[$id] );
 			}
+
 			$groups = $userGroupManager->getUserGroups( $userObject );
 			if ( in_array( 'chatmod', $groups ) || in_array( 'sysop', $groups ) ) {
 				$result->addValue( [ $mName, 'users', $idString ], 'mod', 'true' );
 			}
+
 			$gender = $genderCache->getGenderOf( $userObject );
 			$result->addValue( [ $mName, 'users', $idString, ], 'gender', $gender );
 		}

MediaWikiChat.js
@@ -24,10 +24,6 @@ var MediaWikiChat = {
 		return string.replace( /[^\w\s]|/g, '' ).replace( / /g, '' );
 	},
 
-	ie: function() {
-		return navigator.appVersion.indexOf( 'MSIE' ) != -1;
-	},
-
 	unique: function( array ) {
 		var a = array.concat();
 
@@ -134,7 +130,11 @@ var MediaWikiChat = {
 		}
 		$.ajax( {
 			url: mw.config.get( 'wgScriptPath' ) + '/api.php',
-			data: { 'action': 'chatgetnew', 'format': 'json', 'focussed': focussed },
+			data: {
+				'action': 'chatgetnew',
+				'format': 'json',
+				'focussed': focussed
+			},
 			cache: false
 		} ).done( MediaWikiChat.getNewReply );
 	},
@@ -146,7 +146,11 @@ var MediaWikiChat = {
 
 		for ( var userId in data.users ) {
 			var user = data.users[userId];
-			MediaWikiChat.userData[userId] = { 'name': user.name, 'avatar': user.avatar, 'gender': user.gender };
+			MediaWikiChat.userData[userId] = {
+				'name': user.name,
+				'avatar': user.avatar,
+				'gender': user.gender
+			};
 			if ( user.mod ) {
 				MediaWikiChat.userData[userId].mod = true;
 			}
@@ -465,8 +469,6 @@ var MediaWikiChat = {
 			//'-webkit-filter': 'grayscale(' + hours + ')', /* old webkit */
 			'-webkit-filter': 'grayscale(' + percent + '%)', /* new webkit */
 			'-moz-filter': 'grayscale(' + percent + '%)', /* mozilla */
-			'-ms-filter': 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=' + hours + ')', /* maybe ie */
-			//'filter': 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=' + hours + ')', /* maybe ie */
 			'filter': 'grayscale(' + percent + '%)' /* future */
 		} );
 
@@ -604,7 +606,12 @@ var MediaWikiChat = {
 			$.ajax( {
 				type: 'POST',
 				url: mw.config.get( 'wgScriptPath' ) + '/api.php',
-				data: { 'action': 'chatsendpm', 'message': $( this )[0].value, 'id': toid, 'format': 'json' }
+				data: {
+					'action': 'chatsendpm',
+					'message': $( this )[0].value,
+					'id': toid,
+					'format': 'json'
+				}
 			} ).done( function() {
 				MediaWikiChat.getNew();
 				MediaWikiChat.restartInterval();
@@ -638,7 +645,7 @@ var MediaWikiChat = {
 			if ( mw.user.options.get( 'chat-ping-message' ) ) {
 				MediaWikiChat.audio( 'message' );
 			}
-			document.title = "* " + MediaWikiChat.title;
+			document.title = '* ' + MediaWikiChat.title;
 			if ( mw.user.options.get( 'chat-notify-message' ) ) {
 				MediaWikiChat.notify( title, message );
 			}
@@ -689,7 +696,7 @@ var MediaWikiChat = {
 
 	audio: function( filename ) {
 		var audio = document.createElement( 'audio' );
-		var path = mw.config.get( 'wgScriptPath') + '/extensions/MediaWikiChat/assets/' + filename;
+		var path = mw.config.get( 'wgExtensionAssetsPath' ) + '/MediaWikiChat/assets/' + filename;
 
 		var source = document.createElement( 'source' );
 		source.type = 'audio/ogg';
@@ -709,7 +716,7 @@ var MediaWikiChat = {
 			return;
 		}
 
-		if ( Notification.permission !== "granted" ) {
+		if ( Notification.permission !== 'granted' ) {
 			Notification.requestPermission();
 		}
 
@@ -759,7 +766,11 @@ $( function() {
 			$.ajax( {
 				type: 'POST',
 				url: mw.config.get( 'wgScriptPath' ) + '/api.php',
-				data: { 'action': 'chatsend', 'message': message, 'format': 'json' }
+				data: {
+					'action': 'chatsend',
+					'message': message,
+					'format': 'json'
+				}
 			} ).done( function( msg ) {
 				MediaWikiChat.getNewReply( msg );
 				$( '#mwchat-loading' ).attr(
@@ -807,7 +818,7 @@ $( function() {
 		$( '#mwchat-me' ).animate( { 'top': height }, 'fast' );
 	} );
 
-	$( '#mwchat-topic a').attr( 'target', '_blank'); // Open any link in chat-topic in a new tab
+	$( '#mwchat-topic a' ).attr( 'target', '_blank' ); // Open any link in chat-topic in a new tab
 
 	$( 'input[name=autoscroll]' ).change( function() {
 		if ( this.checked ) {

includes/MediaWikiChat.php
@@ -20,9 +20,9 @@ class MediaWikiChat {
 	 * Get the current UNIX time with 100th seconds (i.e. 138524180871).
 	 * Standard UNIX timestamp contains only 10 digits.
 	 *
-	 * @return int current UNIX timestamp + 100th seconds
+	 * @return int Current UNIX timestamp + 100th seconds
 	 */
-	static function now() {
+	public static function now() {
 		return (int)( microtime( true ) * 100 );
 	}
 
@@ -32,7 +32,7 @@ class MediaWikiChat {
 	 * @param int $id user ID
 	 * @return string avatar image path
 	 */
-	static function getAvatar( $id ) {
+	public static function getAvatar( $id ) {
 		global $wgUploadPath;
 
 		$avatar = new wAvatar( $id, 's' );
@@ -47,21 +47,22 @@ class MediaWikiChat {
 	 * @param UserIdentity $user user that has been blocked/unblocked
 	 * @param UserIdentity $performer user that did the blocking/unblocking
 	 */
-	static function sendSystemBlockingMessage( $type, UserIdentity $user, UserIdentity $performer ) {
+	public static function sendSystemBlockingMessage( $type, UserIdentity $user, UserIdentity $performer ) {
 		$dbw = wfGetDB( DB_PRIMARY );
 
-		$toid = $user->getId();
-		$fromid = $performer->getId();
+		$toId = $user->getId();
+		$fromId = $performer->getId();
 		$timestamp = self::now();
 
 		$dbw->insert(
 			'chat',
 			[
-				'chat_to_id' => $toid,
-				'chat_user_id' => $fromid,
+				'chat_to_id' => $toId,
+				'chat_user_id' => $fromId,
 				'chat_timestamp' => $timestamp,
 				'chat_type' => $type
-			]
+			],
+			__METHOD__
 		);
 	}
 
@@ -69,48 +70,49 @@ class MediaWikiChat {
 	 * Get the list of users who are online, if we have the "chat" user right.
 	 *
 	 * @param User $user
-	 * @return int[]|false array of user IDs and user names on success, boolean false
-	 * if the current user doesn't have the "chat" right
+	 * @return int[]|false Array of user IDs and user names on success, boolean false
+	 *  if the current user doesn't have the "chat" right
 	 */
-	static function getOnline( User $user ) {
+	public static function getOnline( User $user ) {
 		global $wgChatOnlineTimeout;
 
-		if ( $user->isAllowed( 'chat' ) ) {
-			$dbr = wfGetDB( DB_REPLICA );
-
-			$now = self::now();
-			$timestamp = $now - $wgChatOnlineTimeout;
-
-			$res = $dbr->select(
-				'chat_users',
-				[ 'cu_user_id', 'cu_away' ],
-				[
-					"cu_timestamp > $timestamp",
-					"cu_user_id != {$user->getId()}"
-				],
-				__METHOD__
-			);
-
-			$data = [];
-
-			foreach ( $res as $row ) {
-				$away = $row->cu_away;
-
-				$data[$row->cu_user_id] = $now - $away; // number of microseconds since user was last seen
-			}
-			return $data;
-		} else {
+		if ( !$user->isAllowed( 'chat' ) ) {
 			return false;
 		}
+
+		$dbr = wfGetDB( DB_REPLICA );
+
+		$now = self::now();
+		$timestamp = $now - $wgChatOnlineTimeout;
+
+		$res = $dbr->select(
+			'chat_users',
+			[ 'cu_user_id', 'cu_away' ],
+			[
+				"cu_timestamp > $timestamp",
+				"cu_user_id != {$user->getId()}"
+			],
+			__METHOD__
+		);
+
+		$data = [];
+
+		foreach ( $res as $row ) {
+			$away = $row->cu_away;
+
+			$data[$row->cu_user_id] = $now - $away; // number of microseconds since user was last seen
+		}
+
+		return $data;
 	}
 
 	/**
-	 * Is the current user online or not?
+	 * Is the given user online or not?
 	 *
 	 * @param UserIdentity $user
-	 * @return bool whether they're online or not.
+	 * @return bool Whether they're online or not.
 	 */
-	static function amIOnline( UserIdentity $user ) {
+	public static function amIOnline( UserIdentity $user ) {
 		global $wgChatOnlineTimeout;
 
 		$dbr = wfGetDB( DB_REPLICA );
@@ -133,9 +135,9 @@ class MediaWikiChat {
 	/**
 	 * Get interval to poll the server from. Based on the average milliseconds between recent messages.
 	 *
-	 * @return int polling interval to use (how long between each poll)
+	 * @return int Polling interval to use (how long between each poll)
 	 */
-	static function getInterval() {
+	public static function getInterval() {
 		$dbr = wfGetDB( DB_REPLICA );
 		$maxInterval = 30 * 1000;
 		$minInterval = 3 * 1000;
@@ -179,7 +181,7 @@ class MediaWikiChat {
 	 * @param User $user current user object
 	 * @return string parsed message
 	 */
-	static function parseMessage( $message, $user ) {
+	public static function parseMessage( $message, $user ) {
 		global $wgChatRichMessages, $wgChatUseStyleAttribute;
 
 		$smileyString = wfMessage( 'smileys' )->plain();
@@ -283,7 +285,7 @@ class MediaWikiChat {
 	 *
 	 * Prevents speeds slowing down due to massive IM tables
 	 */
-	static function deleteEntryIfNeeded() {
+	public static function deleteEntryIfNeeded() {
 		$dbr = wfGetDB( DB_REPLICA );
 		$field = $dbr->selectField(
 			'chat',
@@ -312,13 +314,14 @@ class MediaWikiChat {
 	 *
 	 * @param UserIdentity $user
 	 */
-	static function updateAway( UserIdentity $user ) {
+	public static function updateAway( UserIdentity $user ) {
 		$dbw = wfGetDB( DB_PRIMARY );
 
 		$dbw->update(
 			'chat_users',
 			[ 'cu_away' => self::now() ],
-			[ 'cu_user_id' => $user->getId() ]
+			[ 'cu_user_id' => $user->getId() ],
+			__METHOD__
 		);
 	}
 }

includes/MediaWikiChatHooks.php
@@ -10,12 +10,13 @@ class MediaWikiChatHooks {
 	 *
 	 * Whenever a user is added to or removed from the 'blockedfromchat' group,
 	 * this function ensures that the chat database table is updated accordingly.
+	 *
 	 * @param User $user
 	 * @param array $add
 	 * @param array $remove
 	 * @param User $performer
 	 */
-	public static function onUserRights( $user, array $add, array $remove, User $performer ) {
+	public static function onUserGroupsChanged( $user, array $add, array $remove, User $performer ) {
 		if ( in_array( 'blockedfromchat', $add ) ) {
 			MediaWikiChat::sendSystemBlockingMessage( MediaWikiChat::TYPE_BLOCK, $user, $performer );
 		}
@@ -27,23 +28,25 @@ class MediaWikiChatHooks {
 
 	/**
 	 * Hook for update.php
+	 *
 	 * @param DatabaseUpdater $updater
 	 */
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
 		$dir = __DIR__ . '/../sql/';
 
-		$updater->addExtensionTable( 'chat', $dir . 'chat.sql', true );
-		$updater->addExtensionTable( 'chat_users', $dir . 'chat_users.sql', true );
+		$updater->addExtensionTable( 'chat', $dir . 'chat.sql' );
+		$updater->addExtensionTable( 'chat_users', $dir . 'chat_users.sql' );
 		$updater->addExtensionField( 'chat_users', 'cu_away', $dir . 'cu_away.sql' );
 		$updater->modifyExtensionField( 'chat_users', 'cu_away', $dir . 'cu_away_new.sql' );
 	}
 
 	/**
 	 * Hook for adding a sidebar portlet ($wgChatSidebarPortlet)
+	 *
 	 * @param Skin $skin
 	 * @param array &$bar
 	 */
-	public static function fnNewSidebarItem( Skin $skin, &$bar ) {
+	public static function onSkinBuildSidebar( Skin $skin, &$bar ) {
 		global $wgChatSidebarPortlet;
 
 		if (
@@ -70,9 +73,6 @@ class MediaWikiChatHooks {
 						$style .= "-webkit-filter: grayscale(1); /* old webkit */
 							-webkit-filter: grayscale(100%); /* new webkit */
 							-moz-filter: grayscale(100%); /* safari */
-							-ms-filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1); /* maybe ie */
-							filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1); /* maybe ie */
-							filter: gray; /* maybe ie */
 							filter: grayscale(100%); /* future */";
 					}
 					$arr[$id] = [
@@ -96,10 +96,12 @@ class MediaWikiChatHooks {
 	}
 
 	/**
+	 * Register new preference options so that they show up on Special:Preferences.
+	 *
 	 * @param User $user
 	 * @param array[] &$preferences
 	 */
-	static function wfPrefHook( $user, &$preferences ) {
+	public static function onGetPreferences( $user, &$preferences ) {
 		$preferences['chat-fullscreen'] = [
 			'type' => 'toggle',
 			'label-message' => 'tog-chat-fullscreen',

includes/api/ChatGetNewAPI.php
@@ -29,8 +29,7 @@ class ChatGetNewAPI extends ApiBase {
 	/** @inheritDoc */
 	public function getExamplesMessages() {
 		return [
-				'action=chatgetnew'
-				=> 'apihelp-chatgetnew-example-1'
+			'action=chatgetnew' => 'apihelp-chatgetnew-example-1'
 		];
 	}
 }

includes/api/ChatKickAPI.php
@@ -71,8 +71,7 @@ class ChatKickAPI extends ApiBase {
 	/** @inheritDoc */
 	public function getExamplesMessages() {
 		return [
-			'action=chatkick&id=1'
-				=> 'apihelp-chatkick-example-1'
+			'action=chatkick&id=1' => 'apihelp-chatkick-example-1'
 		];
 	}
 

includes/api/ChatSendAPI.php
@@ -27,8 +27,11 @@ class ChatSendAPI extends ApiBase {
 				// Flood check
 				$res = $dbr->selectField(
 					'chat',
-					[ 'count(*)' ],
-					[ "chat_timestamp > " . ( $timestamp - ( $wgChatFloodSeconds * 100 ) ), " chat_user_id = " . $id ],
+					[ 'COUNT(*)' ],
+					[
+						'chat_timestamp > ' . ( $timestamp - ( $wgChatFloodSeconds * 100 ) ),
+						'chat_user_id' => $id
+					],
 					__METHOD__
 				);
 				if ( $res > $wgChatFloodMessages ) {
@@ -88,8 +91,7 @@ class ChatSendAPI extends ApiBase {
 	/** @inheritDoc */
 	public function getExamplesMessages() {
 		return [
-			'action=chatsend&message=Hello%20World!'
-				=> 'apihelp-chatsend-example-1'
+			'action=chatsend&message=Hello%20World!' => 'apihelp-chatsend-example-1'
 		];
 	}
 

includes/api/ChatSendPMAPI.php
@@ -28,8 +28,11 @@ class ChatSendPMAPI extends ApiBase {
 				// Flood check
 				$res = $dbr->selectField(
 					'chat',
-					[ 'count(*)' ],
-					[ "chat_timestamp > " . ( $timestamp - ( $wgChatFloodSeconds * 100 ) ), " chat_user_id = " . $fromId ],
+					[ 'COUNT(*)' ],
+					[
+						'chat_timestamp > ' . ( $timestamp - ( $wgChatFloodSeconds * 100 ) ),
+						'chat_user_id' => $fromId
+					],
 					__METHOD__
 				);
 				if ( $res > $wgChatFloodMessages ) {

commit 3ed752603a545bd8003eec07adfdf201464dd60d

Fix variable name
I broke this in 6c5a368c2fe2429d5055a489d0ba01929a566de3.

Change-Id: I2912a42c59608ddb3a9a188543e9bd6e6c6ecf93

includes/api/GetNewWorker.php
@@ -89,7 +89,7 @@ class GetNewWorker {
 				$message = $row->chat_message;
 
 				$fromId = $row->chat_user_id;
-				$toid = $row->chat_to_id;
+				$toId = $row->chat_to_id;
 
 				if ( $fromId == $user->getId() ) {
 					$convWith = User::newFromId( $toId )->getName();